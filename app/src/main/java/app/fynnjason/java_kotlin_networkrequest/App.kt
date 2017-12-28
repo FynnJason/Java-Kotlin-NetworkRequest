5629b28af2c1a54382a71e226a892bdf6a01d76e
FynnJason <410132905@qq.com>
2017-12-28 16:53:27 +0800

    see log 17.12.28.

---
+++ app/src/main/java/app/fynnjason/java_kotlin_networkrequest/App.kt
@@ -0,0 +1,13 @@
+package app.fynnjason.java_kotlin_networkrequest
+
+import android.app.Application
+
+/**
+ * Created by FynnJason.
+ * Des：App
+ */
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
