e2eeb886bb8f4fb902d326362a8663142e17a7b0
FynnJason <410132905@qq.com>
2017-12-28 17:47:40 +0800

    see log 17.12.28.

--- app/src/main/java/app/fynnjason/java_kotlin_networkrequest/MainActivity.kt
+++ app/src/main/java/app/fynnjason/java_kotlin_networkrequest/MainActivity.kt
@@ -10,8 +10,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        RequestService.getInstance().log()
 
-        text_view.setOnClickListener { RequestService.getInstance().log() }
     }
 }
--- app/src/main/java/app/fynnjason/java_kotlin_networkrequest/java/api/RequestService.java
+++ app/src/main/java/app/fynnjason/java_kotlin_networkrequest/java/api/RequestService.java
@@ -3,9 +3,16 @@ package app.fynnjason.java_kotlin_networkrequest.java.api;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.util.concurrent.TimeUnit;
+
 import app.fynnjason.java_kotlin_networkrequest.java.base.BaseEnity;
 import app.fynnjason.java_kotlin_networkrequest.java.bean.GankBean;
 import io.reactivex.Observable;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by FynnJason.
@@ -13,21 +20,39 @@ import io.reactivex.Observable;
  */
 
 public class RequestService implements ApiService {
-    private RequestService() {
 
+    private static final String BASE_URL ="http://gank.io/api/data/";
+
+    private static ApiService sApiService = null;
+
+    private RequestService() {
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addNetworkInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+                .connectTimeout(60, TimeUnit.SECONDS)
+                .readTimeout(60,TimeUnit.SECONDS)
+                .writeTimeout(60,TimeUnit.SECONDS)
+                .retryOnConnectionFailure(true)
+                .build();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .client(client)
+                .build();
+        sApiService = retrofit.create(ApiService.class);
     }
 
     private static class Holder {
-        private static RequestService Instance = new RequestService();
+        private static RequestService sInstance = new RequestService();
     }
 
     public static RequestService getInstance() {
-        return Holder.Instance;
+        return Holder.sInstance;
     }
 
 
     @Override
     public Observable<BaseEnity<GankBean>> allGank() {
-        return null;
+        return sApiService.allGank();
     }
 }
